dc2003c7b0ef5407b22a191b8d792a2ad5d751c2
Sergey Chepukhin <chepukhin.sergey@gmail.com>
2024-07-19 21:47:07 +0500

    update

--- src/shared/api/routes.ts
+++ src/shared/api/routes.ts
@@ -8,7 +8,7 @@ interface CountryRoutes {
 
 export const routes: CountryRoutes = {
     azerbaijan: {
-        baku: 'https://script.google.com/macros/s/AKfycbwJr2t9mTlWVVRM0cqM3RnpU48qX5ZKEGjzbjAnvuCw0zvjFnpyFGRw-Uu4o2Cv9jrE6A/exec'
+        baku: 'https://script.google.com/macros/s/AKfycbwtw0mu0QStxl9jVn26FQ0KjUETDKP8RYK464IfCoM9hskFmksj3MxLnN2EgTkZ_CeR/exec'
     },
     cyprus: {
         limassol: 'https://script.google.com/macros/s/AKfycbyWeo6GqjKjHKeCUI1EEuJOFpeH_5a0LJzVkgkEU6TNsR60m6MDrdUZrliVpO_K3Inivw/exec'
